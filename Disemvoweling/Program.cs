15ed0701561f854c6c5fac3715c07c3ecde7118e
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-10-02 15:51:41 +0300

    Добавьте файлы проекта.

---
+++ Disemvoweling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disemvoweling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            do
+            {
+                Console.WriteLine("Enter string for Disemvoweling");
+                str = Console.ReadLine();
+                string VOWELS = "AEIOUaeiou";
+                foreach (var item in str)
+                {
+                    foreach (var item2 in VOWELS)
+                    {
+                        if (item == item2)
+                        {
+                            str = str.Replace(item.ToString(), "");
+                        }
+                    }
+                }
+
+                Console.WriteLine(str);
+            } while (str != "");
+
+            Console.ReadLine();
+        }
+    }
+}
