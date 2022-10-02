9abaff4a3d043036f044476a80179917b735c461
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-10-02 16:03:33 +0300

    Добавлены тесты

---
+++ DisemvowelingTests/KataTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Disemvoweling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disemvoweling.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void ShouldRemoveAllVowels()
+        {
+            Assert.AreEqual("Ths wbst s fr lsrs LL!", Kata.Disemvowel("This website is for losers LOL!"));
+        }
+
+        [TestMethod()]
+        public void MultilineString()
+        {
+            Assert.AreEqual("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd", Kata.Disemvowel("No offense but,\nYour writing is among the worst I've ever read"));
+        }
+
+        [TestMethod()]
+        public void OneMoreForGoodMeasure()
+        {
+            Assert.AreEqual("Wht r y,  cmmnst?", Kata.Disemvowel("What are you, a communist?"));
+        }
+    }
+}
